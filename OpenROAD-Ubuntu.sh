2efe4c4dab46bfe5f4f555665b969f37a7589083
Rishabh Jain <2r10j5@gmail.com>
2026-03-27 23:22:54 +0530

    Add files via upload

---
+++ OpenROAD-Ubuntu.sh
@@ -0,0 +1,7 @@
+# Ubuntu 22.04.5
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install -y build-essential cmake git clang bison flex tcl-dev libffi-dev libreadline-dev zlib1g-dev python3 python3-pip python3-venv swig qtbase5-dev libboost-all-dev
+
+sudo apt install -y libx11-dev libxaw7-dev libxmu-dev libxext-dev libglu1-mesa-dev libeigen3-dev
